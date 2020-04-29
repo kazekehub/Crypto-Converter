fd7046ef9fa928e34050a5fcf2909b71969ca93c
Kazybek <kzhapparkulov@gmail.com>
2020-04-30 00:52:43 +0600

    added JSON and rewrite QuoteTableViewController

--- Crypto-Converter/Controller/QuoteTableViewController.swift
+++ Crypto-Converter/Controller/QuoteTableViewController.swift
@@ -17,8 +17,9 @@ class QuoteTableViewController: UITableViewController {
     
     var isSelectQuoteMode = false
     var provider: QuoteProviderProtocol?
-    var dataArray: [Quote] = []
-
+    var quoteData: [Quote] = []
+    var quoteApi = "https://api.nomics.com/v1/currencies/ticker?key=3c8c0907276523d0ff0e94c50657de0c&format=json&interval=5m&convert=USD"
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         NotificationCenter.default.addObserver(self,
@@ -33,15 +34,42 @@ class QuoteTableViewController: UITableViewController {
     }
     
     @IBAction func quoteUpdateClick(_ sender: Any) {
+        loadJSON()
         provider = QuoteProvider(delegate: self)
         provider?.requestQuotes()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadJSON()
         provider = QuoteProvider(delegate: self)
         provider?.requestQuotes()
         tableView.tableFooterView = UIView()
+    }
+    
+    fileprivate func loadJSON() {
+        if let url = URL(string: quoteApi) {
+            let quoteLoadTask =
+                URLSession.shared.dataTask(with: url) {
+                    [weak self]
+                    (data, response, error)
+                    in
+                    guard let self = self else {
+                        return
+                    }
+                    if let data = data {
+                        do {
+                            self.quoteData = try JSONDecoder().decode([Quote].self, from: data)
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        } catch {
+                            print("Decoding JSON failure")
+                        }
+                    }
+            }
+            quoteLoadTask.resume()
+        }
     }
     
     // MARK: - Table view data source
@@ -49,13 +77,13 @@ class QuoteTableViewController: UITableViewController {
         return 100
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray.count
+        return quoteData.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "quotesCellId", for: indexPath) as! QuoteTableViewCell
         
-        let quote = dataArray[indexPath.row]
+        let quote = quoteData[indexPath.row]
         cell.quoteImage.image = UIImage(named: quote.logoUrl)
         cell.quoteRankLabel.text = "\(quote.rank)"
         cell.quoteSymbolLabel.text = quote.symbol
@@ -68,7 +96,7 @@ class QuoteTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let cell = sender as? QuoteTableViewCell
         let indexPath = tableView.indexPath(for: cell!)
-        let quote = dataArray[indexPath!.row]
+        let quote = quoteData[indexPath!.row]
         
         if isSelectQuoteMode == true {
             NotificationCenter.default.post(name: NotificationSendSelectedQuote, object: quote)
@@ -83,7 +111,7 @@ class QuoteTableViewController: UITableViewController {
 
 extension QuoteTableViewController: QuoteProviderDelegate {
     func provideQuotes(quotes: [Quote]) {
-        dataArray = quotes
+        quoteData = quotes
         tableView.reloadData()
     }
 }
